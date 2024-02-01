cc03217686d05d6fb453e99669d927537f5965f9
_ChingC <chingc.ccmail@gmail.com>
2024-02-01 09:33:54 +0800

    feat: SettingsUniqueKey

---
+++ NotificationCenter/Migrations/20240201013303_SettingsUniqueKey.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificationCenter.Migrations
+{
+    /// <inheritdoc />
+    public partial class SettingsUniqueKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "SystemSettings",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SystemSettings_Key",
+                table: "SystemSettings",
+                column: "Key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SystemSettings_Key",
+                table: "SystemSettings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "SystemSettings",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
